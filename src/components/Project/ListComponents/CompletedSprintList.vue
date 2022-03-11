fd72d7e0e9aa8b008dea4b215614473e09c87c90
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-03-12 01:03:25 +0530

    added ui changes to completed sprint list component

--- src/components/Project/ListComponents/CompletedSprintList.vue
+++ src/components/Project/ListComponents/CompletedSprintList.vue
@@ -7,17 +7,20 @@
       :items="desserts"
       :single-expand="singleExpand"
       :expanded.sync="expanded"
-      item-key="name"
+      item-key="id"
       show-expand
       hide-default-footer
       class="elevation-0"
     >
+    
       <template v-slot:top>
 
         <v-toolbar flat>
 
           <v-toolbar-title style="font-weight:bold;">COMPLETED</v-toolbar-title>
           <v-spacer></v-spacer>
+
+          <v-btn color=" primary ">ADD TASK</v-btn>
 
           <!-- <v-switch
             v-model="singleExpand"
@@ -28,6 +31,20 @@
         </v-toolbar>
 
       </template>
+
+      <template v-slot:[`item.action`]="{  }">
+        <div class="dflex">
+            <v-btn icon x-small>
+                <v-icon>mdi-plus</v-icon>
+            </v-btn>
+
+            <v-btn icon x-small>
+                <v-icon>mdi-delete</v-icon>
+            </v-btn>
+
+        </div>
+      </template>
+
 
       <template v-slot:expanded-item="{ headers, item }">
         <td :colspan="headers.length">More info about {{ item.name }}</td>
@@ -43,102 +60,79 @@
   export default {
     data () {
       return {
+
         expanded: [],
+
         singleExpand: false,
+
         dessertHeaders: [
-          {
-            text: 'Task',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          { text: 'ASSIGNEE', value: 'calories' },
-          { text: 'DUE DATE', value: 'fat' },
-          { text: 'PRIORITY', value: 'carbs' },
+            {
+                text: 'Task',
+                align: 'start',
+                sortable: false,
+                value: 'task',
+            },
+            { text: 'ASSIGNEE', value: 'assignee' },
+            { text: 'DUE DATE', value: 'due_date' },
+            { text: 'PRIORITY', value: 'priority' },
+            { text: 'ACTION', value: 'action' },
         ],
+
         desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%',
-          },
+          
+            {
+                id : 1,
+                task: 'Authentication Application creation for future usage',
+                assignee: 'Naveen | Lahiru',
+                due_date: '22/2/2022',
+                priority: 'High',
+            },
+
+            {
+                id : 2,
+                task: 'Authentication Application creation for future usage',
+                assignee: 'Naveen | Lahiru',
+                due_date: '22/2/2022',
+                priority: 'High',
+            },
+
+            {
+                id : 3,
+                task: 'Authentication Application creation for future usage',
+                assignee: 'Naveen | Lahiru',
+                due_date: '22/2/2022',
+                priority: 'High',
+            },
+
+
+            {
+                id : 4,
+                task: 'Authentication Application creation for future usage',
+                assignee: 'Naveen | Lahiru',
+                due_date: '22/2/2022',
+                priority: 'High',
+            },
+
+            {
+                id : 5,
+                task: 'Authentication Application creation for future usage',
+                assignee: 'Naveen | Lahiru',
+                due_date: '22/2/2022',
+                priority: 'High',
+            },
+
+            {
+                id : 6,
+                task: 'Authentication Application creation for future usage',
+                assignee: 'Naveen | Lahiru',
+                due_date: '22/2/2022',
+                priority: 'High',
+            },
+
         ],
+      
       }
+    
     },
   }
 </script>
