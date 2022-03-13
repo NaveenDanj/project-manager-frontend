a43a960d23d0379f5e8904001dfda4cad5f0bce4
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-03-14 00:37:42 +0530

    added notification page layoyut

--- src/router/index.js
+++ src/router/index.js
@@ -15,13 +15,12 @@ const routes = [
     component: () =>import("../views/Project.vue"),
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/notification",
+    name: "Notification",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () =>import("../views/Notification.vue"),
   },
 ];
 
---
+++ src/views/Notification.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <div class="home">
+
+    <UpperMenuBar />
+
+    <v-card class="justify-center mx-auto mt-5" style="width : 65%" >
+
+      <label>Notification</label>
+
+    </v-card>
+
+    
+  </div>
+
+</template>
+
+<script>
+import UpperMenuBar from '@/components/WorkSpace/UpperMenuBar.vue';
+
+
+export default {
+
+  components : {
+    UpperMenuBar,
+  }
+
+}
+</script>
+
+<style scoped>
+
+  .home{
+    background-color: #EEEEEE;
+    min-height : 100vh;
+  }
+
+
+</style>
