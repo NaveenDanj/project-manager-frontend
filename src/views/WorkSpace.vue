51a7a57738b6ba9b5cf82ba358dc1908ef97e9a7
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-03-14 03:26:32 +0530

    added add new user component

--- src/views/WorkSpace.vue
+++ src/views/WorkSpace.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="home">
     <UpperMenuBar />
-    
-    <div class="pa-3">
-      <h3>Workspace</h3>
-    </div>
 
+    <div class="pa-5">
+      <h3>Work space Settings</h3>
+      <br />
+
+      <AddNewUser />
+
+    </div>
 
   </div>
 </template>
@@ -13,12 +16,15 @@
 <script>
 // @ is an alias to /src
 import UpperMenuBar from '@/components/WorkSpace/UpperMenuBar.vue';
+import AddNewUser from '@/components/WorkSpace/AddNewUser.vue';
+
 
 export default {
   name: "Home",
 
   components: {
     UpperMenuBar,
+    AddNewUser
   },
 
 };
