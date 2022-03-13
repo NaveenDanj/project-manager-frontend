d2c8f62e5aa3dc76f2091dc23081dbb6135db4f9
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-03-14 00:23:59 +0530

    added create task header component

--- src/components/Project/Dialogs/CreateTaskDialog.vue
+++ src/components/Project/Dialogs/CreateTaskDialog.vue
@@ -3,7 +3,6 @@
   <v-dialog
     v-model="dialog"
     max-width="650px"
-    scrollable
     transition="dialog-bottom-transition"
   >
     <template v-slot:activator="{ on, attrs }">
@@ -31,36 +30,8 @@
         </v-toolbar-items>
       </v-toolbar>
 
-      <div class="pa-3 d-flex justify-space-between grow">
-        <div class="my-auto">
-          <v-btn color="green" dark>
-            IN REVIEW
-            <v-icon>mdi-arrow-right-thin</v-icon>
-          </v-btn>
-        </div>
-
-        <div class="justify-space-between d-flex" style="width: auto">
-          <div class="mr-1">
-            <v-avatar class="my-auto mr-1" color="red" dark size="35">
-              <label style="color: white; font-weight: bold">L</label>
-            </v-avatar>
-
-            <v-avatar class="my-auto" color="purple" dark size="35">
-              <label style="color: white; font-weight: bold">N</label>
-            </v-avatar>
-          </div>
-
-          <v-avatar class="my-auto" outline dark size="35">
-            <v-btn icon>
-              <v-icon>mdi-flag</v-icon>
-            </v-btn>
-          </v-avatar>
-
-          <v-btn icon>
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </div>
-      </div>
+      <!-- task header -->
+      <CreateTaskHeader />
 
       <v-divider></v-divider>
 
@@ -76,90 +47,10 @@
           value="Cloud web project creation"
         ></v-text-field>
 
-        <v-textarea placeholder="Enter your task description here..." outlined>
-
-        </v-textarea>
-
+        <v-textarea placeholder="Enter your task description here..." outlined></v-textarea>
 
         <!-- task meta data -->
-        <div class="pa-0">
-          <table
-            border="1"
-            style="width: 100%; border-collapse: collapse; border-radius: 5px"
-          >
-            <tbody>
-              <tr>
-                <td class="pa-1">
-                  <v-icon class="ml-1 mr-1">mdi-comment</v-icon>
-                  <label style="font-weight:bold;">Comments</label>
-                </td>
-                <td class="pa-1">
-
-                  <textarea
-                    style="width: 100%; border: 1px solid black; padding-left: 5px"
-                    type="text"
-                    rows="1"
-                  ></textarea>
-                </td>
-              </tr>
-
-              <tr>
-                <td>
-                  <v-icon class="ml-1 mr-1">mdi-heart-remove</v-icon>
-                  <label style="font-weight:bold;">Risk</label>
-                </td>
-
-                <td class="pa-1">
-                  <select
-                    style="width: 100%; border: 1px solid black; padding-left: 5px"
-                  >
-                    <option>Very Likely</option>
-                    <option>Likely</option>
-                    <option>At Risk</option>
-                    <option>High Risk</option>
-                    <option>Low Risk</option>
-                  </select>
-                </td>
-
-              </tr>
-
-              <tr>
-                <td>
-                  <v-icon class="ml-1 mr-1"> mdi-flag-checkered </v-icon>
-                  <label style="font-weight:bold;">Sprint Goal</label>
-                </td>
-
-                <td class="pa-1">
-                  <select
-                    style="width: 100%; border: 1px solid black; padding-left: 5px"
-                  >
-                    <option>Goal 1</option>
-                    <option>Goal 2</option>
-                  </select>
-                </td>
-
-              </tr>
-
-              <tr>
-                <td>
-                  <v-icon class="ml-1 mr-1"> mdi-clipboard-text-clock</v-icon>
-                  <label style="font-weight:bold;">Due Date</label>
-                </td>
-
-                <td class="pa-1">
-                  <input
-                    type="date"
-                    style="width: 100%; border: 1px solid black; padding-left: 5px"
-                  />
-                  
-                </td>
-
-              </tr>
-
-            </tbody>
-
-          </table>
-        </div>
+        <TaskMetaData />
 
         <!-- sub task list -->
         <TodoList />
@@ -175,12 +66,16 @@
 <script>
 
 import TodoList from './components/TodoList.vue'
+import TaskMetaData from './components/TaskMetaData.vue'
+import CreateTaskHeader from './components/CreateTaskHeader.vue'
 
 
 export default {
 
   components : {
-    TodoList
+    TodoList,
+    TaskMetaData,
+    CreateTaskHeader
   },
 
   data() {
---
+++ src/components/Project/Dialogs/components/CreateTaskHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="pa-3 d-flex justify-space-between grow">
+    <div class="my-auto">
+      <v-btn color="green" dark>
+        IN REVIEW
+        <v-icon>mdi-arrow-right-thin</v-icon>
+      </v-btn>
+    </div>
+
+    <div class="justify-space-between d-flex" style="width: auto">
+      <div class="mr-1">
+        <v-avatar class="my-auto mr-1" color="red" dark size="35">
+          <label style="color: white; font-weight: bold">L</label>
+        </v-avatar>
+
+        <v-avatar class="my-auto" color="purple" dark size="35">
+          <label style="color: white; font-weight: bold">N</label>
+        </v-avatar>
+      </div>
+
+      <v-avatar class="my-auto" outline dark size="35">
+        <v-btn icon>
+          <v-icon>mdi-flag</v-icon>
+        </v-btn>
+      </v-avatar>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </div>
+  </div>
+</template>
